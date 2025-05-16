ab434489aadeed933913dc39672638e62ea1a350
ivan11sa <ivan11asir@gmail.com>
2025-05-16 20:53:25 +0200

    arreglosss

--- Sanchez_Acuna_Ivan_Proyecto_IAW/db_connection.php
+++ Sanchez_Acuna_Ivan_Proyecto_IAW/db_connection.php
@@ -5,17 +5,16 @@
 //  Definimos los datos de conexión a la base de datos.
 $host = getenv('DB_HOST') ?: 'mysql-service';       // Servidor donde está la base de datos en XAMPP
 $user = getenv('DB_USER') ?: 'root'; // Nombre de la base de datos que vamos a usar
-$pass = getenv('DB_PASSWORD') ?: 'root';              // Usuario de la base de datos 
-$name = getenv('DB_NAME') ?: 'gestion_incidencias';             // Contraseña del usuario. En nuestro caso no la tenemos establecida. 
+$password = getenv('DB_PASSWORD') ?: 'root';              // Usuario de la base de datos 
+$db = getenv('DB_NAME') ?: 'gestion_incidencias';             // Contraseña del usuario. En nuestro caso no la tenemos establecida. 
 
 //  Creamos la conexión a la base de datos usando MySQL.
-$conn = new mysqli($host, $user, $pass, $name);
-if ($conn->connect_error) {
-    die("Error de conexión (".$conn->connect_errno."): ".$conn->connect_error);
-}
-
+$conn = new mysqli($host, $user, $password, $db);
 //  Configuramos el juego de caracteres a UTF-8 para evitar problemas con acentos y caracteres especiales.
 $conn->set_charset("utf8mb4");
-
+if ($conn->connect_error) {
+    header('Location: error.php?error=db_connection');
+    exit();
+    }
 
 ?>
